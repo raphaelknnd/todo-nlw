d05a7de7e2f928450c14742ac9946175c04d06b9
raphaelknnd <raphakennedy.dev@gmail.com>
2024-09-12 18:26:47 -0300

    implemented 'Metas abertas'

--- index.js
+++ index.js
@@ -56,8 +56,24 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas (" + realizadas.length + ")",
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+    })
+
+    if(abertas.length == 0){
+        console.log("Não existem metas abertas :D")
+        return
+    }
+
+    await select({
+        message: "Metas abertas (" + abertas.length + ")",
+        choices: [...abertas]
     })
 }
 
@@ -80,6 +96,10 @@ const start =  async () => {
                     value: 'realizadas'
                 },
                 {
+                    name: 'Metas abertas',
+                    value: 'abertas'
+                },
+                {
                     name: 'Remover meta',
                     value: 'remover'
                 },
@@ -100,6 +120,10 @@ const start =  async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
+                break
             case "remover":
                 console.log("vamos remover")
                 break
